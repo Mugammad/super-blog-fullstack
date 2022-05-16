506affbd32df294b291f2c36756e6c9df0a19bc6
Xola13 <xolamagatya86@gmail.com>
2022-05-16 12:20:09 +0200

    commit

--- super-blog-api/index.js
+++ super-blog-api/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = require('./routes')
+const Cors = require('cors')
 
 const app = express()
 app.use(express.json())
 
+app.use(Cors)
 app.use(router)
+
+
+app.all("*", function (req, res, next) {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+   res.setHeader("Access-Control-Allow-Credentials", true); 
+   next();
+});
+
 
 app.get('/', (req, res) => {
     res.status(200).json({message: "Welcome to Super Blog's API"})
